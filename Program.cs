dc8817e58552e7a0b754a38d275c177e71a90a74
Roger Wood <roger.wood@gmail.com>
2013-12-26 15:58:44 -0700

    Changed from .net 4.5 to .net 2.0

--- Program.cs
+++ Program.cs
@@ -2,12 +2,10 @@
 using System;
 using System.Collections.Generic;
 using System.IO;
-using System.Linq;
 using System.Reflection;
 using System.Runtime.InteropServices;
 using System.Text;
 using System.Threading;
-using System.Threading.Tasks;
 using System.Windows.Forms;
 
 namespace WallpaperChange
