71dd7e0fc9d49de29f1c13dbc8706246acf2e5db
Roger Wood <roger.wood@gmail.com>
2017-01-09 13:07:00 -0700

    Added about box, started adding squirrel update support.

--- Properties/AssemblyInfo.cs
+++ Properties/AssemblyInfo.cs
@@ -6,13 +6,11 @@ using System.Runtime.InteropServices;
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
 [assembly: AssemblyTitle("WallpaperChange")]
-[assembly: AssemblyDescription("")]
+[assembly: AssemblyDescription("Changes your wallpaper when you want it to. Goto: https://github.com/rwood/WallpaperChange")]
 [assembly: AssemblyConfiguration("")]
-[assembly: AssemblyCompany("")]
+//[assembly: AssemblyCompany("Roger Wood")]
 [assembly: AssemblyProduct("WallpaperChange")]
-[assembly: AssemblyCopyright("Copyright ©  2013")]
-[assembly: AssemblyTrademark("")]
-[assembly: AssemblyCulture("")]
+[assembly: AssemblyCopyright("Copyright © Roger Wood 2017")]
 
 // Setting ComVisible to false makes the types in this assembly not visible 
 // to COM components.  If you need to access a type in this assembly from 
@@ -32,5 +30,5 @@ using System.Runtime.InteropServices;
 // You can specify all the values or you can default the Build and Revision Numbers 
 // by using the '*' as shown below:
 // [assembly: AssemblyVersion("1.0.*")]
-[assembly: AssemblyVersion("1.0.0.0")]
-[assembly: AssemblyFileVersion("1.0.0.0")]
+[assembly: AssemblyVersion("0.9.1.*")]
+//[assembly: AssemblyFileVersion("0.9.1.*")]
--- SysTrayApplicationContext.cs
+++ SysTrayApplicationContext.cs
@@ -1,9 +1,15 @@
 ﻿using System;
 using System.ComponentModel;
+using System.Diagnostics;
 using System.Drawing;
+using System.IO;
+using System.Linq;
+using System.Net.Mime;
 using System.Reflection;
+using System.Threading.Tasks;
 using System.Timers;
 using System.Windows.Forms;
+using Squirrel;
 using WallpaperChange.Settings;
 using Timer = System.Timers.Timer;
 
@@ -12,6 +18,7 @@ namespace WallpaperChange
     internal class SysTrayApplicationContext : ApplicationContext
     {
         private readonly object _syncLock = new object();
+        private ToolStripMenuItem _btnAbout;
         private ToolStripMenuItem _btnSettings;
         private ToolStripMenuItem _btnStop;
         private ContextMenuStrip _contextMenuStrip1;
@@ -41,6 +48,7 @@ namespace WallpaperChange
             components = new Container();
             _notifyIcon1 = new NotifyIcon(components);
             _contextMenuStrip1 = new ContextMenuStrip(components);
+            _btnAbout = new ToolStripMenuItem();
             _btnStop = new ToolStripMenuItem();
             _btnSettings = new ToolStripMenuItem();
             _contextMenuStrip1.SuspendLayout();
@@ -54,11 +62,15 @@ namespace WallpaperChange
             _notifyIcon1.Tag = "WallpaperChange";
             _notifyIcon1.Text = @"WallpaperChange";
             _notifyIcon1.Visible = true;
-            _contextMenuStrip1.Items.AddRange(new ToolStripItem[] {_btnSettings, _btnStop});
+            _contextMenuStrip1.Items.AddRange(new ToolStripItem[] {_btnAbout, _btnSettings, _btnStop});
             _contextMenuStrip1.Name = "_contextMenuStrip1";
             _contextMenuStrip1.Size = new Size(176, 76);
+            _btnAbout.Name = "_btnAbout";
+            _btnAbout.AutoSize = true;
+            _btnAbout.Text = @"About";
+            _btnAbout.Click += btnAbout_Click;
             _btnStop.Name = "_btnStop";
-            _btnSettings.AutoSize = true;
+            _btnStop.AutoSize = true;
             _btnStop.Text = @"Stop";
             _btnStop.Click += btnStop_Click;
             _btnSettings.Name = "_btnSettings";
@@ -71,6 +83,11 @@ namespace WallpaperChange
             _timer.Elapsed += _timer_Elapsed;
             _timer.Interval = 10000;
             _timer.Start();
+        }
+
+        private void btnAbout_Click(object sender, EventArgs e)
+        {
+            new AboutBox().ShowDialog();
         }
 
         private void _timer_Elapsed(object sender, ElapsedEventArgs e)
@@ -94,6 +111,30 @@ namespace WallpaperChange
             finally
             {
                 _timer.Start();
+            }
+        }
+
+        private async Task CheckForUpdates()
+        {
+            if (File.Exists("..\\Update.exe"))
+            {
+                using (var mgr = new UpdateManager("http://tamarau.com/SybilClient"))
+                {
+                    var updateInfo = await mgr.CheckForUpdate();
+                    if (updateInfo != null)
+                    {
+                        Trace.WriteLine("Current Version: " + updateInfo.CurrentlyInstalledVersion.Version);
+                        Trace.WriteLine("Newest Version: " + updateInfo.FutureReleaseEntry.Version);
+                        Trace.WriteLine(updateInfo.ReleasesToApply.Count() + " releases to apply");
+                        if (updateInfo.ReleasesToApply.Any())
+                        {
+                            Trace.WriteLine("Getting Updates");
+                            await mgr.UpdateApp();
+                            Trace.WriteLine("Updates Applied, Please Restart Sybil Client");
+                            MessageBox.Show("Updates Applied, Please Restart Sybil Client");
+                        }
+                    }
+                }
             }
         }
 
